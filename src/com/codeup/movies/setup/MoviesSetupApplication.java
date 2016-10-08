b65ef3acdc79a357e16d58767ab527f4bd5d8464
Luis Montealegre <montealegreluis@gmail.com>
2016-10-08 15:18:43 -0500

    Use a properties file to configure the application.

--- src/com/codeup/movies/setup/MoviesSetupApplication.java
+++ src/com/codeup/movies/setup/MoviesSetupApplication.java
@@ -2,18 +2,27 @@ package com.codeup.movies.setup;
 
 import com.codeup.db.MySQLConnection;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
+import java.util.Properties;
 
 public class MoviesSetupApplication {
     public static void main(String[] args) {
-        MySQLConnection databaseConnection = new MySQLConnection(
-            "root", "Codeup1!"
-        );
+        Properties config = new Properties();
+        MySQLConnection databaseConnection = null;
 
         try {
+
+            config.load(new FileInputStream("config.properties"));
+
+            databaseConnection = new MySQLConnection(
+                config.getProperty("user"),
+                config.getProperty("password")
+            );
+
             Connection connection = databaseConnection.connect();
             System.out.println("Creating movies database...");
-            new MoviesDatabase(connection).create("movies_db");
+            new MoviesDatabase(connection).create(config.getProperty("database"));
             System.out.println("Creating tables...");
             new MoviesMigration(connection).up();
             System.out.println("Seeding database...");
