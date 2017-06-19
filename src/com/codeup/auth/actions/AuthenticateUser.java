984f80fa109adddf45658a74c09739fa3b290ba2
Luis Montealegre <montealegreluis@gmail.com>
2017-06-19 13:52:20 -0500

    Refactor and test the AuthenticateUser action
    * Add tests for this action
    * Simplify condition when the authentication fails

--- src/com/codeup/auth/actions/AuthenticateUser.java
+++ src/com/codeup/auth/actions/AuthenticateUser.java
@@ -16,15 +16,12 @@ public class AuthenticateUser {
 
     public boolean attemptLogin(String username, String password) {
         User user = users.identifiedBy(username);
-        if (user == null) {
-            return false;
-        }
-        if (user.passwordMatch(password)) {
-            this.user = user;
-            return true;
-        }
 
-        return false;
+        if (user == null || !user.passwordMatch(password)) return false;
+
+        this.user = user;
+
+        return true;
     }
 
     public User user() {
