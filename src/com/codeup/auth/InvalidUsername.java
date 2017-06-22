c22a42b5c6391732a47ebd93adb4eb8937ec494c
Luis Montealegre <montealegreluis@gmail.com>
2017-06-21 22:04:18 -0500

    Encapsulate the rules for usernames
    * The Username class enforces the following rules
      - A username cannot be null or empty
      - A username allows alphanumeric characters, dots, underscores and dashes
    * Add test cases describing those rules

---
+++ src/com/codeup/auth/InvalidUsername.java
@@ -0,0 +1,22 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+package com.codeup.auth;
+
+public class InvalidUsername extends IllegalArgumentException {
+
+    private InvalidUsername(String format) {
+        super(format);
+    }
+
+    public static InvalidUsername withEmpty(String value) {
+        return new InvalidUsername(String.format("Username cannot be empty, found '%s'", value));
+    }
+
+    public static InvalidUsername with(String value) {
+        return new InvalidUsername(String.format(
+            "A username can only contain alphanumeric characters dots, dashes & underscores, %s found",
+            value
+        ));
+    }
+}
